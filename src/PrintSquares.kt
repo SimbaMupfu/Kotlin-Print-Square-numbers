137ba8b088b3c4cce9fb25acf7d9f86c408c2430
simba_m <scmupfururirwa@gmail.com>
2021-03-29 14:12:32 +0200

    show how to print sqaure numbers in an array

---
+++ src/PrintSquares.kt
@@ -0,0 +1,7 @@
+
+    fun main(args: Array<String>){
+        val squares = Array(51) { i -> i  }
+        for(i in 0 until(squares.size)){
+            println("The square for $i is ${squares[i]}")
+        }
+    }
