942d76a2a915b341a9f22ee08f05872c2650d18f
simba_m <scmupfururirwa@gmail.com>
2021-03-29 16:14:23 +0200

    access array elements using it indices

--- src/PrintSquares.kt
+++ src/PrintSquares.kt
@@ -8,4 +8,13 @@
         println(squares.component5())
         println(squares.elementAt(9))
         println(squares.elementAtOrElse(55) { "Not Available" })
+
+        //check if the square is odd or even
+        for(i in squares.indices){
+            if(i % 2 == 0){
+                println("The square of $i is an odd number")
+            }else{
+                println("The square of $i is an even number")
+            }
+        }
     }
